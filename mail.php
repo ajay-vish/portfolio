000c0a91c3ddd4dd72661f2a99156e51208be956
ajay-sokrati <avishwakarma@merkleinc.com>
2022-05-08 17:43:28 +0530

    Correcting information 1

--- mail.php
+++ mail.php
@@ -7,6 +7,5 @@ $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
 $message = "Name: $name \nEmail: $email \nText: $text";
-alert($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient")
 $pagetitle = "New message from the \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
